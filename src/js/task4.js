045e7987bd78ffc2a90162ac4983fe008ef604da
borinskikh <causelesscause@gmail.com>
2021-02-10 18:41:12 +0300

    update

--- src/js/task4.js
+++ src/js/task4.js
@@ -2,55 +2,63 @@
     document.getElementById('main').appendChild(getTask());
     document.getElementById('task4-input1').value = '100';
     document.getElementById('task4-input2').value = '100';
-    //submit4();
+    submit();
 
 
     function getTask() {
-        const output = document.createElement('div');
-        output.setAttribute('id', 'task4');
-        output.setAttribute('class', 'task bg-dark');
-        output.innerHTML = '<h3 class="text">task 4</h3>';
-        const form = document.createElement('form');
+        const task = document.createElement('div');
+        task.setAttribute('id', 'task4');
+        task.setAttribute('class', 'task bg-dark d-flex flex-column');
+        task.innerHTML = '<h3 class="text">task 4</h3>';
+        const content = document.createElement('div');
+        content.setAttribute('id', 'task3-content');
+        content.setAttribute('class', 'm-3 d-flex flex-column flex-lg-row justify-content-around flex-fill');
+        const form = document.createElement('div');
         form.setAttribute('id', 'task4-form');
-        form.setAttribute('class', 'form');
+        form.setAttribute('class', 'm-5 d-flex flex-column');
         const field1 = document.createElement('div');
         field1.setAttribute('id', 'task4-field1');
+        field1.setAttribute('class', 'd-flex flex-column');
         const input1 = document.createElement('input');
         input1.setAttribute('type', 'text');
         input1.setAttribute('id', 'task4-input1');
-        input1.setAttribute('class', 'input');
+        input1.setAttribute('class', 'bg-dark text-light border-secondary');
+        input1.setAttribute('placeholder', ' Enter a number');
         const label1 = document.createElement('label');
-        label1.innerHTML = 'Enter a number between 100 and 300<br>';
+        label1.innerHTML = 'Enter a number between 100 and 300';
         label1.setAttribute('for', 'task4-input1');
-        label1.setAttribute('class', 'text');
-        field1.appendChild(label1);
-        field1.appendChild(input1);
         const field2 = document.createElement('div');
         field2.setAttribute('id', 'task4-field2');
+        field2.setAttribute('class', 'd-flex flex-column');
         const input2 = document.createElement('input');
         input2.setAttribute('type', 'text');
         input2.setAttribute('id', 'task4-input2');
-        input2.setAttribute('class', 'input');
-        field1.appendChild(input2);
-        const button = document.createElement('div');
+        input2.setAttribute('class', 'bg-dark text-light border-secondary');
+        input2.setAttribute('placeholder', ' Enter a number');
+        const button = document.createElement('button');
         button.innerHTML = 'Submit';
-        button.setAttribute('class', 'btn  btn-dark');
         button.setAttribute('id', 'task4-button');
+        button.setAttribute('class', 'm-3 btn btn-secondary');
+        button.addEventListener('click',
+            () => { submit(); }
+        );
         const imageWrapper = document.createElement('div');
         imageWrapper.setAttribute('id', 'task4-image');
-        imageWrapper.setAttribute('style', 'max-width: 300px; max-height: 300px;');
-        output.appendChild(form);
-        output.appendChild(imageWrapper);
+        imageWrapper.setAttribute('class', 'd-flex flex-row flex-wrap');
+
+        task.appendChild(content);
+        content.appendChild(form);
+        content.appendChild(imageWrapper);
         form.appendChild(field1);
         form.appendChild(field2);
         form.appendChild(button);
-        button.addEventListener('click',
-            () => { submit4(); }
-        );
-        return output;
+        field1.appendChild(label1);
+        field1.appendChild(input2);
+        field1.appendChild(input1);
+        return task;
     }
 
-    function submit4() {
+    function submit() {
         const number1 = parseInt(document.getElementById('task4-input1').value);
         const number2 = parseInt(document.getElementById('task4-input2').value);
         clearInput();
@@ -79,10 +87,13 @@
     function displayResults(imageUrl) {
         clearChildren(document.getElementById('task4-image'));
         const card = document.getElementById('task4-image');
-        card.setAttribute('class', 'card');
+        card.setAttribute('class', 'm-2 card bg-dark');
         const image = document.createElement('img');;
         image.setAttribute('src', imageUrl);
+        image.setAttribute('class', 'card-img-top p-2');
+        image.setAttribute('style', 'object-fit: scale-down; max-height: 300px; width: auto;');
         card.appendChild(image);
+        clearInput();
     }
 
     function clearChildren(parent) {
